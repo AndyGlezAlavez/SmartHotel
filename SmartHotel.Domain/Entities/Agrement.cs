8ff336849cba93c9311f099d2cbe6c015991f4c9
emmdyron <dayronrdiaz@gmail.com>
2025-05-08 17:37:53 -0700

    Montando agrement, falta ver como desarrollar la idea del pago del cliente para generar el acuerdo

---
+++ SmartHotel.Domain/Entities/Agrement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHotel.Domain.Common;
+using SmartHotel.Domain.Entities;
+
+namespace SmartHotel.Domain.Entities
+{
+    public class Agrement : Entity
+    {
+      #region Properties
+      
+        /// Nombre del cliente 
+        /// 
+
+        public string ClientName { get; set; }
+        
+        /// Correo del cliente
+        /// 
+        public string Clientemail { get; set; }
+
+        // Fecha de inicio
+        public DateTime FechaInicio { get; set; }
+
+        // Fecha final
+
+        public DateTime FechaFinal { get; set; }
+
+        #endregion
+
+
+        public Agrement(string clientname, string clientemail,DateTime fechaInicio, DateTime fechaFinal, Guid id) : base(id)
+        {   ClientName = clientname;
+            Clientemail = clientemail;
+                FechaInicio = fechaInicio;
+                FechaFinal = fechaFinal;
+            }
+
+            public TimeSpan DuracionRenta()
+            {
+                return FechaFinal - FechaInicio;
+           
+        }
+
+
+
+
+    }
+}
