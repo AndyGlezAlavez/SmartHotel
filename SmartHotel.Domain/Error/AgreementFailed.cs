fa5af3bacbaa3e3642b05ea6997cbf4b9c4f7400
Ariel <ariel.herrera.rodeiro@gmail.com>
2025-05-16 22:31:07 -0400

    Agregando posibles errores

---
+++ SmartHotel.Domain/Error/AgreementFailed.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+
+namespace EquipmentMonitoring.Domain.Errors
+{
+    public static class AgreementFailed
+    {
+        public static Error CannotExecuteAgreement =>
+            new Error("The agreement failed");
+    }
+}
---
+++ SmartHotel.Domain/Error/IsNotRentable.cs
@@ -0,0 +1,13 @@
+﻿using FluentResults;
+
+namespace EquipmentMonitoring.Domain.Errors
+{
+    public static class IsNotRentable
+    {
+        public static Error CannotExecuteExternalOperation =>
+            new Error("This room si not rentabled.");
+
+        public static Error CannotExecuteAgreementOperation =>
+            new Error("This room si not rentabled in this period");
+    }
+}
