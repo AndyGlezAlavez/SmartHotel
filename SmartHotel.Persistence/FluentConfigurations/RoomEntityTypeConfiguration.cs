e0968f60a3d38cfac23768353b78210d638c125f
emmdyron <dayronrdiaz@gmail.com>
2025-05-20 11:31:59 -0700

    Empezando a montar RoomEntityType... Esta comentado el OwnsOne... Hay que arreglarlo

---
+++ SmartHotel.Persistence/FluentConfigurations/RoomEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using SmartHotel.Domain.Entities;
+using SmartHotel.Persistence.FluentConfigurations.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SmartHotel.Persistence.FluentConfigurations
+{
+    internal class RoomEntityTypeConfiguration
+        : EntityTypeConfigurationBase<Room>
+    {
+        public override void Configure(EntityTypeBuilder<Room> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("Rooms");
+            //builder.OwnsOne(x => x.Address);       HAY QUE ARREGLAR ESTOOOOO
+        }
+    }
+}
