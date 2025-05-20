f664012609e4c44d99003ce3486685ed24b61a73
emmdyron <dayronrdiaz@gmail.com>
2025-05-20 10:42:02 -0700

    Montando el inicio de la BD

---
+++ SmartHotel.Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartHotel.Domain;
+
+
+namespace SmartHotel.Persistence.Contexts
+{
+    public class AppDbContext: DbContext
+    {
+        //#region Tables
+        
+       
+
+
+
+
+      
+
+
+    }
+}
