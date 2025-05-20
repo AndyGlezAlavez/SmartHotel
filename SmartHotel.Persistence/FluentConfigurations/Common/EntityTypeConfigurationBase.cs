0da7f09f83f203142c4c03dec5af3196b78164c7
emmdyron <dayronrdiaz@gmail.com>
2025-05-20 11:12:21 -0700

    montando el common

---
+++ SmartHotel.Persistence/FluentConfigurations/Common/EntityTypeConfigurationBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHotel.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SmartHotel.Persistence.FluentConfigurations.Common
+{
+    public abstract class EntityTypeConfigurationBase <T> 
+        : IEntityTypeConfiguration<T>
+        where T : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).IsRequired();
+        }
+    }
+}
