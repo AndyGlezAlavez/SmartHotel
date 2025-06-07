629dd6d06ee73a3862c04d9636ab31f98123f19e
Ariel <ariel.herrera.rodeiro@gmail.com>
2025-06-07 15:43:52 -0400

    Avanzando smoke

---
+++ SmartHotel.Application/Commands/Smoke/UpdateSmoke/UpdateSmokeCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHotel.Domain.Types;
+using SmartHotel.Application.Common;
+
+namespace SmartHotel.Application.Commands.Smoke.UpdateSmoke
+{
+    public sealed record UpdateSmokeCommand(SmartHotel.Domain.Entities.Smoke Smoke) : ICommand
+    {
+
+    }
+}
