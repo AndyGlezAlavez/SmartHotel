f3af339cba2af8590d5635b9914833f83b556425
Jose <Johanna@DESKTOP-KB8JB41>
2025-06-17 00:46:23 -0400

    Diseño de la ventana de ¨Reservaciones¨

---
+++ SmartHotel.VisualApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartHotel.VisualApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public AgreementDetails Agreement { get; set; }
+        public ObservableCollection<AgreementDetails> Agreements { get; set; }
+
+        public MainWindow()
+        {
+            Agreement = new AgreementDetails(new DateTime(2001, 11, 06), new DateTime(01, 11, 09), "Ariel");
+            Agreements = new ObservableCollection<AgreementDetails>()
+            {
+                new AgreementDetails(new DateTime(2025, 11, 06), new DateTime(2025, 11, 09), "Andy"),
+                new AgreementDetails(new DateTime(2025, 11, 07), new DateTime(2025, 11, 15), "Dayron"),
+                new AgreementDetails(new DateTime(2025, 11, 10), new DateTime(2025, 11, 13), "Jose"),
+            };
+
+            //DataContext = this;
+
+
+            InitializeComponent();
+            AgreementsGrid.ItemsSource = Agreements;
+        }
+    }
+}
